c5f25811fddbae9eacc0b914941c578226ace38c
G2_Diego <elv-diego.ptn@eduge.ch>
2022-03-28 13:23:07 +0200

    First ajax start views

--- shop/views.py
+++ shop/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from .models import Produit
 from .forms import ProduitForm
-
-
+import json
+from django.http import JsonResponse
 # Create your views here.
 
 def home(request):
@@ -19,4 +19,17 @@ def ajout_voiture(request):
 
     return render(request, 'shop/ModelForm.html', {'form' : form})
 
+def search_car(request):
+    if request.method == 'POST':
+        search_str = json.loads(request.body).get('searchText')
+
+        produits = Produit.filter(
+            marque__starts_with = search_str) | Produit.filter(
+            modele__starts_with = search_str) | Produit.filter(
+            description__icontains = search_str) | Produit.filter(
+            categorie__starts_with = search_str)
+
+        data = produits.values()
+
+        return JsonResponse(list(data),safe=False)
 
